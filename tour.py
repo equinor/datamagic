9536a314e09f277305bccf1d51ff4322907b0e47
Olve Maudal <olve.maudal@gmail.com>
2022-02-12 00:54:49 +0000

    Set Northern Lights as default URL

--- tour.py
+++ tour.py
@@ -2,8 +2,8 @@ import os
 import collections
 import azure.storage.blob
 
-URL = r'https://datavillagesa.blob.core.windows.net/volve?sv=2018-03-28&sr=c&sig=2CCvEBPWiojChDE1N%2BJw%2ByZ%2Fn5oMf3iXSEUZBm5XMq8%3D&se=2022-03-13T23%3A04%3A36Z&sp=rl'
-#URL = r'https://datavillagesa.blob.core.windows.net/northernlights?sv=2018-03-28&sr=c&sig=VTWTxWY%2BT7KQ8Y3m93%2B298%2FUjVMi6ebEyEee%2Ffu16SY%3D&se=2022-03-03T22%3A16%3A48Z&sp=rl'
+#URL = r'https://datavillagesa.blob.core.windows.net/volve?sv=2018-03-28&sr=c&sig=2CCvEBPWiojChDE1N%2BJw%2ByZ%2Fn5oMf3iXSEUZBm5XMq8%3D&se=2022-03-13T23%3A04%3A36Z&sp=rl'
+URL = r'https://datavillagesa.blob.core.windows.net/northernlights?sv=2018-03-28&sr=c&sig=VTWTxWY%2BT7KQ8Y3m93%2B298%2FUjVMi6ebEyEee%2Ffu16SY%3D&se=2022-03-03T22%3A16%3A48Z&sp=rl'
 
 container = azure.storage.blob.ContainerClient.from_container_url(URL)
 
