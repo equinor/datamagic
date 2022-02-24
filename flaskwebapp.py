38e70cf05fb0f57c0750be9ae1b9645bde20848e
Olve Maudal <olve.maudal@gmail.com>
2022-02-24 20:45:13 +0000

    Fix linting

--- flaskwebapp.py
+++ flaskwebapp.py
@@ -1,10 +1,15 @@
+"""Flask webapp demo."""
+
 import flask
 
 app = flask.Flask(__name__)
 
+
 @app.route('/')
 def index():
+    """Return a default HTML response."""
     return "Data Magic!"
+
 
 if __name__ == '__main__':
     app.debug = True
