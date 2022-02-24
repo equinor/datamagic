1df6381072dcaceaf59a0953590061e69edbf18b
Olve Maudal <olve.maudal@gmail.com>
2022-02-24 20:40:50 +0000

    Rename to flaskwebapp

---
+++ flaskwebapp.py
@@ -0,0 +1,15 @@
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Data Magic!"
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
+
+# hint:
+# export FLASK_APP=flaskappdemo.py
+# flask run
