7cdfe7b2fd1340396f256003e87497f073c2ebf4
Olve Maudal <olve.maudal@gmail.com>
2022-02-24 20:56:51 +0000

    Clean up

--- flaskwebapp.py
+++ flaskwebapp.py
@@ -7,14 +7,21 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    """Return a default HTML response."""
+    """Return a default response."""
     return "Data Magic!"
 
 
-if __name__ == '__main__':
+def main():
+    """Run main function of the program."""
     app.debug = True
     app.run()
 
+
+if __name__ == '__main__':
+    main()
+
 # hint:
-# export FLASK_APP=flaskappdemo.py
-# flask run
+#   export FLASK_APP=flaskwebapp.py
+#   flask run
+# or
+#   python flaskwebapp.py
