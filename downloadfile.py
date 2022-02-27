c0fcbe8f05aeaf4a1bc2af392b84326526f01a9c
Olve Maudal <olve.maudal@gmail.com>
2022-02-27 20:45:14 +0000

    Lint file

--- downloadfile.py
+++ downloadfile.py
@@ -1,35 +1,44 @@
+"""Download file from a storage container."""
+
 import os
 import sys
-import azure.storage.blob
+import azure.storage.blob as asb
+
 
 def download_file(container_url, path):
-    container = azure.storage.blob.ContainerClient.from_container_url(container_url)
+    """Download file a storage container."""
+    container = asb.ContainerClient.from_container_url(container_url)
     filename = os.path.basename(path)
     blob_client = container.get_blob_client(path)
     if blob_client.exists():
-        bytes = blob_client.download_blob().readall()
+        data = blob_client.download_blob().readall()
         with open(filename, "wb") as file:
-            file.write(bytes)
+            file.write(data)
     else:
         print(f'File not found: {path}')
         return 1
     return 0
 
-if __name__ == '__main__':
 
-    if len(sys.argv) == 1:
-        print(f"usage: {sys.argv[0]} [FILE]... - download FILE(s) from container")
-        print(f"Hint: Environment variable CONTAINER_URL must be defined")
-        sys.exit(1)
+def main(argv):
+    """Parse arguments and run program."""
+    if len(argv) == 1:
+        print(f"usage: {argv[0]} [FILE]... - download FILE(s) from container")
+        print("Hint: Environment variable CONTAINER_URL must be defined")
+        return 1
 
-    URL = os.environ.get('CONTAINER_URL')
-    if URL == None:
+    url = os.environ.get('CONTAINER_URL')
+    if not url:
         print('Please specify the container in CONTAINER_URL')
-        sys.exit(1)
+        return 1
 
-    for filename in sys.argv[1:]:
-        retval = download_file(URL, filename)
+    for filename in argv[1:]:
+        retval = download_file(url, filename)
         if retval != 0:
             sys.exit(retval)
 
-    sys.exit(0)
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
