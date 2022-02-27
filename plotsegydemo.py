01d6cf3ca31efc962744cb53ea7807c11a9b80b7
Olve Maudal <olve.maudal@gmail.com>
2022-02-27 20:43:33 +0000

    Lint file

--- plotsegydemo.py
+++ plotsegydemo.py
@@ -1,9 +1,10 @@
+"""Demonstrate segyio."""
+
 import segyio
 import matplotlib.pyplot as plt
 
 traces = []
-filename = 'VSPZO_RAW_2020-01-17_4.SEGY'
-with segyio.open(filename, strict=False) as f:
+with segyio.open('VSPZO_RAW_2020-01-17_4.SEGY', strict=False) as f:
     for trace in f.trace:
         traces.append(list(trace))
 plt.imshow(traces, vmin=-0.01, vmax=0.01)
