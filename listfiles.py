6e8e38e84252b62187e221184d146f0036d11e2e
Olve Maudal <olve.maudal@gmail.com>
2022-03-01 11:17:04 +0000

    Rightadjust size

--- listfiles.py
+++ listfiles.py
@@ -8,4 +8,4 @@ URL = os.environ['CONTAINER_URL']
 container = azure.storage.blob.ContainerClient.from_container_url(URL)
 
 for blob in container.list_blobs():
-    print(f'{blob.size:<20}{blob.name}')
+    print(f'{blob.size:>20} {blob.name}')
