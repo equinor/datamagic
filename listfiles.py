8ea92df6dee3774c9ded830ef20b8480b392fe88
Olve Maudal <olve.maudal@gmail.com>
2022-03-24 15:39:03 +0000

    Restore files after course

--- listfiles.py
+++ listfiles.py
@@ -1,9 +1,8 @@
 import os
 import azure.storage.blob
 
-url = os.environ['CONTAINER_URL']
-
-container = azure.storage.blob.ContainerClient.from_container_url(url)
+URL = os.environ['CONTAINER_URL']
+container = azure.storage.blob.ContainerClient.from_container_url(URL)
 
 for blob in container.list_blobs():
-    print(f"{blob.size:<20} {blob.name}")
+    print(f'{blob.size:<20} {blob.name}')
