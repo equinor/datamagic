d7ca2787a62669543087a40b8eb35f7c342402e0
Olve Maudal <olve.maudal@gmail.com>
2022-04-20 09:03:38 +0000

    Add listfiles.py

---
+++ listfiles.py
@@ -0,0 +1,9 @@
+import os
+import azure.storage.blob
+
+url = os.environ['CONTAINER_URL']
+
+container = azure.storage.blob.ContainerClient.from_container_url(url)
+
+for blob in container.list_blobs():
+    print(f"{blob.size:<20}{blob.name}")
