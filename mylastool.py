3d38dc266318c5033a1c2de51460cb82917c0499
Olve Maudal <olve.maudal@gmail.com>
2022-04-20 10:21:30 +0000

    Add initial version of mylastool.py

---
+++ mylastool.py
@@ -0,0 +1,24 @@
+import os
+import azure.storage.blob
+
+def get_container():
+    url = os.environ['CONTAINER_URL']
+    return azure.storage.blob.ContainerClient.from_container_url(url)
+
+def get_list_of_lasfiles(container):
+    files = []
+    for blob in container.list_blobs():
+        if blob.name.endswith('.LAS'):
+            files.append(blob.name)
+    return files
+
+def print_list_of_lasfiles(container):
+    lasfiles = get_list_of_lasfiles(container)
+    for file in lasfiles:
+        print(file)
+
+if __name__ == '__main__':
+    container = get_container()
+    print_list_of_lasfiles(container)
+
+
