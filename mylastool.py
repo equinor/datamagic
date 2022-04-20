8bab8adee1489d7c390608932cca6ef7bf7934b9
Olve Maudal <olve.maudal@gmail.com>
2022-04-20 11:12:15 +0000

    Become PEP8 compliant (step 1)

--- mylastool.py
+++ mylastool.py
@@ -1,24 +1,34 @@
 import os
 import azure.storage.blob
 
+
 def get_container():
+    """Create container from CONTAINER_URL."""
     url = os.environ['CONTAINER_URL']
     return azure.storage.blob.ContainerClient.from_container_url(url)
 
+
 def get_list_of_lasfiles(container):
+    """Get list of LAS files in a container."""
     files = []
     for blob in container.list_blobs():
         if blob.name.endswith('.LAS'):
             files.append(blob.name)
     return files
 
+
 def print_list_of_lasfiles(container):
     lasfiles = get_list_of_lasfiles(container)
     for file in lasfiles:
         print(file)
 
+
+def is_probably_not_a_LAS_file(filename):
+    return not filename.endswith(".LAS")
+
+
 def read_lasfile(container, filename):
-    if not filename.endswith(".LAS"):
+    if is_probably_not_a_LAS_file(filename):
         raise OSError("Probably not a LAS file")
     blob_client = container.get_blob_client(filename)
     data = blob_client.download_blob().content_as_bytes()
@@ -26,6 +36,7 @@ def read_lasfile(container, filename):
     for line in data.splitlines():
         lines.append(line.decode("ascii", errors="ignore"))
     return lines
+
 
 def find_section_index(lines, prefix):
     idx = 0
@@ -35,25 +46,31 @@ def find_section_index(lines, prefix):
         idx += 1
     return idx
 
+
 def get_header_section(lines):
     return lines[:find_section_index(lines, "~A")]
 
+
 def get_data_section(lines):
     return lines[find_section_index(lines, "~A"):]
+
 
 def print_header_section(lines):
     for line in get_header_section(lines):
         print(line)
 
+
 def print_data_section(lines):
     for line in get_data_section(lines):
         print(line)
+
 
 def main():
     container = get_container()
     lines = read_lasfile(container, "31_5-7 Eos/07.Borehole_Seismic/TZV_TIME_SYNSEIS_2020-01-17_2.LAS")
     print_header_section(lines)
     #print_data_section(lines)
+
 
 if __name__ == '__main__':
     main()
