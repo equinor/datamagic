c7b4f61ef109b271a5a968bbc8829073b514ef19
Olve Maudal <olve.maudal@gmail.com>
2022-07-04 09:35:05 +0000

    Delinting my Python tool

--- mylastool.py
+++ mylastool.py
@@ -1,22 +1,32 @@
+"""My LAS tool."""
+
 import os
 import azure.storage.blob
 
+
 def get_container():
+    """Get a handle to an Azure Storage Blob container."""
     url = os.environ['CONTAINER_URL']
     return azure.storage.blob.ContainerClient.from_container_url(url)
 
+
 def get_list_of_lasfiles(container):
+    """Get list of LAS files in a container."""
     files = []
     for blob in container.list_blobs():
         if blob.name.endswith('.LAS'):
             files.append(blob.name)
     return files
 
+
 def print_list_of_lasfiles(container):
+    """Print pretty directory listing of LAS files in a container."""
     for name in get_list_of_lasfiles(container):
         print(name)
 
+
 def main():
+    """My LAS file tool."""
     container = get_container()
     print_list_of_lasfiles(container)
 
