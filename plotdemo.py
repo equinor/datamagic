da6c19e93b4b0c854c01b28ef842940892cb64cb
Olve Maudal <olve.maudal@gmail.com>
2022-07-04 07:33:21 +0000

    Prepare for course

--- plotdemo.py
+++
@@ -1,5 +0,0 @@
-import matplotlib.pyplot as plt
-
-numbers = [4, 6, 2, 3, 5, 6, 4]
-plt.plot(numbers)
-plt.savefig('plotdemo.png')
--- theanswer.py
+++ theanswer.py
@@ -1,4 +1,4 @@
 a = 6
 b = 9
-c = a * (b - 2)
+c = a * (b - 3)
 print(f"The answer is {c}!")
