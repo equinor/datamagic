964c448ea1d4ac86a8577a4702b68a96b0c9fb16
Olve Maudal <olve.maudal@gmail.com>
2022-07-04 09:29:49 +0000

    Extract get list of lasfiles

--- mylastool.py
+++ mylastool.py
@@ -5,10 +5,16 @@ def get_container():
     url = os.environ['CONTAINER_URL']
     return azure.storage.blob.ContainerClient.from_container_url(url)
 
-def print_list_of_lasfiles(container):
+def get_list_of_lasfiles(container):
+    files = []
     for blob in container.list_blobs():
         if blob.name.endswith('.LAS'):
-            print(f"{blob.size:<20}{blob.name}")
+            files.append(blob.name)
+    return files
+
+def print_list_of_lasfiles(container):
+    for name in get_list_of_lasfiles(container):
+        print(name)
 
 def main():
     container = get_container()
