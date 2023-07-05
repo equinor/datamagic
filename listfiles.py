71cea57668531cd5a04cf41cd07027fe23032a68
Olve Maudal <olve.maudal@gmail.com>
2023-07-05 09:51:44 +0200

    Prepare for course

--- listfiles.py
+++ listfiles.py
@@ -1,13 +1,9 @@
 import os
 import azure.storage.blob
 
-def main():
-    url = os.environ['CONTAINER_URL']
+url = os.environ['CONTAINER_URL']
 
-    container = azure.storage.blob.ContainerClient.from_container_url(url)
+container = azure.storage.blob.ContainerClient.from_container_url(url)
 
-    for blob in container.list_blobs():
-        print(f'{blob.size:<20}{blob.name}')
-
-if __name__ == "__main__":
-    main()
+for blob in container.list_blobs():
+    print(f'{blob.size:<20}{blob.name}')
